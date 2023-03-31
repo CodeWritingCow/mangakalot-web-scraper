94a6e633a9b9c6010caa9a07bb5de2526dbeac2c
Gary Pang <gary.pang@gmail.com>
2023-03-31 02:01:02 -0400

    Remove console logs and unused code

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ async function parseMangakalotComics(url) {
         });
 
         // Mangakalot currently renders comic images inside div.container-chapter-reader
-        await page.goto(url); //, { waitUntil: 'networkidle2' }
+        await page.goto(url);
         await page.waitForSelector('div.container-chapter-reader', {
             timeout: 100
         });
@@ -37,42 +37,19 @@ async function parseMangakalotComics(url) {
         const mangaName = urlParts[url.split('/').indexOf('chapter') + 1];
         const mangaChapter = urlParts.length - 1;
 
-        // if user runs `node index.js`
-        // cwdPath is Users/USER_NAME/Documents/GitHub/mangakalot-web-scraper/images/shiji/chapter_1/
-        // executablePath is /Users/USER_NAME/.nvm/versions/node/v16.15.0/bin/images/shiji/chapter_1
-
-        // if user runs `dist/index-macos`
-        // cwdPath is /snapshot/dist/images/shiji/chapter_1/        
-        // executablePath is /Users/USER_NAME/Documents/GitHub/mangakalot-web-scraper/dist/images/shiji/chapter_1
+        // if user runs executable, use executablePath. Otherwise, use cwdPath.
         const executablePath = path.join(path.dirname(process.execPath), 'images', mangaName, urlParts[mangaChapter]);
         const cwdPath = path.join(process.cwd(), 'images', mangaName, urlParts[mangaChapter]);
         const saveFolderPath = cwdPath.includes('snapshot') ? executablePath : cwdPath;
 
-        console.log('cwdPath', cwdPath);
-        console.log('executablePath', executablePath);
-        console.log('saveFolderPath', saveFolderPath);
-
-        // console.log('process', process);
-        // console.log('process.pkg', process.pkg);
-        // console.log('process.cwd()', process.cwd());
-        // console.log('__dirname', __dirname);
-        // console.log('process.execPath', process.execPath);
-        // console.log('path.dirname(process.execPath)', path.dirname(process.execPath));
-
         // Create /images/manga_name/chapter_number directory if it doesn't exist
-        fs.mkdir(
-            saveFolderPath,
-            { recursive: true },
-            (err) => {
-                if (err) {
-                    return console.error(err);
-                } else {
-                    console.log(
-                        `Creating directory ${saveFolderPath}/`
-                    );
-                }
+        fs.mkdir(saveFolderPath, { recursive: true }, (err) => {
+            if (err) {
+                return console.error(err);
+            } else {
+                console.log(`Creating directory ${saveFolderPath}/`);
             }
-        );
+        });
 
         // Save images inside manga chapter subfolder
         for (const src of imageSrcs) {
