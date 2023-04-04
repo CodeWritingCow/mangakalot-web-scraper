4c3eb91a5861b158249ee63fda98a128d5e14009
Gary Pang <gary.pang@gmail.com>
2023-04-04 02:45:43 -0400

    Check if user enters valid Mangakalot URL
    Write function checkUserInput to check if user enters valid Mangakalot
    URL.
    Write helper functions printIntroMessage and quitProgram.

--- getUserInput.js
+++ getUserInput.js
@@ -14,24 +14,48 @@ const ask = (query) => {
 };
 
 const getUserInput = async () => {
+    printIntroMessage();
+
+    const mangaUrl = await ask('Enter URL: ');
+
+    return checkUserInput(mangaUrl);
+};
+
+const checkUserInput = async (url) => {
+    if (url.includes(`https://mangakakalot.com`)) {
+        rl.close;
+        return url;
+    } else if (url === 'q') {
+        quitProgram();
+    } else {
+        console.log('\n');
+        console.log('URL is invalid!');
+
+        const mangaUrl = await ask('Please enter a valid Mangakalot URL: ');
+
+        return checkUserInput(mangaUrl);
+    }
+};
+
+function printIntroMessage() {
     console.log('\n');
     console.log('Welcome to Mangakalot Web scraper!');
     console.log('\n');
     console.log('Enter URL of of manga chapter to download');
     console.log('(ex. https://mangakakalot.com/chapter/shiji/chapter_6)\n');
+    console.log(`(To quit, enter 'q' or press ctrl + c)`);
+    console.log('\n');
+}
 
-    const mangaUrl = await ask('Enter URL: ');
-    // TODO: Add logic for validating mangaUrl as a Mangakalot URL 
-
-    rl.close;
-    return mangaUrl;
-};
-
-rl.on('close', () => {
+function quitProgram() {
     console.log('\n');
     console.log('Exiting Mangakalot Web scraper!');
     console.log('\n');
     process.exit(0);
+}
+
+rl.on('close', () => {
+    quitProgram();
 });
 
 module.exports = getUserInput;
